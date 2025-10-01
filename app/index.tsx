011375a21c0eb666cfa3b12ea4b1025ebc2f98da
Duong <nguyendinhduong2005yt@gmail.com>
2025-10-01 23:05:07 +0700

    btvn

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function index() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Index</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
